96941d071337ed6a99af5015c26f343984e0e8b9
Nayela <nayelawick@gmail.com>
2021-03-03 18:50:26 +0000

    WIP - making cursor trail sprinkles. WIP because when I put the JS in a script tag on the view page it works fine, but when I add it to application.js via a custom js file, it does not work...

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("custom/cursor_sprinkles")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -14,3 +16,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+console.log("This is the application.js calling")
